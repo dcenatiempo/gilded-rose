d07caeca866165f94dc4812420e251f958b795c1
Devin Cenatiempo <devin.cenatiempo@divvypay.com>
2022-11-15 14:29:29 -0700

    update tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -13,8 +13,15 @@ function getItems() {
   return items;
 }
 
+function updateItems(items, times) {
+  let x = times
+  while (x > 0) {
+    update_quality(items);
+    x -= 1;
+  }
+}
 
-describe("Gilded Rosee 1 update", () => {
+describe("Gilded Rosee List of items", () => {
   const items = getItems();
   update_quality(items);
 
@@ -51,96 +58,120 @@ describe("Gilded Rosee 1 update", () => {
 
   it("Conjured Mana Cake", () => {
     expect(items[8].sell_in).toBe(2);
-    expect(items[8].quality).toBe(12);
-  });
-});
-
-describe("Gilded Rose 2 updates", () => {
-  const items = getItems()
-
-  update_quality(items);
-  update_quality(items);
-
-  it("normal", () => {
-    expect(items[0].sell_in).toBe(8);
-    expect(items[0].quality).toBe(18);
-  });
-
-  it("Aged Brie", () => {
-    expect(items[1].sell_in).toBe(0);
-    expect(items[1].quality).toBe(2);
-  });
-
-  it("Elixir of the Mongoose", () => {
-    expect(items[2].sell_in).toBe(3);
-    expect(items[2].quality).toBe(5);
-  });
-
-  it("Sulfuras, Hand of Ragnaros", () => {
-    expect(items[3].sell_in).toBe(0);
-    expect(items[3].quality).toBe(80);
-    expect(items[4].sell_in).toBe(-1);
-    expect(items[4].quality).toBe(80);
-  });
-
-  it("Backstage passes to a TAFKAL80ETC concert", () => {
-    expect(items[5].sell_in).toBe(13);
-    expect(items[5].quality).toBe(22);
-    expect(items[6].sell_in).toBe(8);
-    expect(items[6].quality).toBe(50);
-    expect(items[7].sell_in).toBe(3);
-    expect(items[7].quality).toBe(50);
-  });
-
-  it("Conjured Mana Cake", () => {
-    expect(items[8].sell_in).toBe(1);
     expect(items[8].quality).toBe(11);
   });
 });
 
+describe("Normal", () => {
 
-describe("Gilded Rose update 16 times", () => {
-  const items = getItems()
+  it("2 updates", () => {
+    const items = [new Item("normal", 10, 20)]
+    updateItems(items, 2)
 
-  let x = 16
-  while (x > 0) {
-    update_quality(items);
-    x -= 1;
-  }
+    expect(items[0].sell_in).toBe(8);
+    expect(items[0].quality).toBe(18);
+  });
 
-  it("normal", () => {
+  it("16 updates", () => {
+    const items = [new Item("normal", 10, 20)]
+    updateItems(items, 16)
+
     expect(items[0].sell_in).toBe(-6);
     expect(items[0].quality).toBe(0);
+  })
+})
+
+describe("Aged Brie", () => {
+
+  it("2 updates", () => {
+    const items = [new Item("Aged Brie", 2, 0)]
+    updateItems(items, 2)
+
+    expect(items[0].sell_in).toBe(0);
+    expect(items[0].quality).toBe(2);
   });
 
-  it("Aged Brie", () => {
-    expect(items[1].sell_in).toBe(-14);
-    expect(items[1].quality).toBe(30);
+  it("16 updates", () => {
+    const items = [new Item("Aged Brie", 2, 0)]
+    updateItems(items, 16)
+
+    expect(items[0].sell_in).toBe(-14);
+    expect(items[0].quality).toBe(30);
+  })
+})
+
+describe("Sulfuras", () => {
+
+  it("2 updates", () => {
+    const items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80), new Item("Sulfuras, Hand of Ragnaros", -1, 80)]
+    updateItems(items, 2)
+
+    expect(items[0].sell_in).toBe(0);
+    expect(items[0].quality).toBe(80);
+    expect(items[1].sell_in).toBe(-1);
+    expect(items[1].quality).toBe(80);
   });
 
-  it("Elixir of the Mongoose", () => {
+  it("16 updates", () => {
+    const items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80), new Item("Sulfuras, Hand of Ragnaros", -1, 80)]
+
+    updateItems(items, 16)
+
+    expect(items[0].sell_in).toBe(0);
+    expect(items[0].quality).toBe(80);
+    expect(items[1].sell_in).toBe(-1);
+    expect(items[1].quality).toBe(80);
+  })
+})
+
+describe("Backstage", () => {
+
+  it("2 updates", () => {
+    const items = []
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+    updateItems(items, 2)
+
+    expect(items[0].sell_in).toBe(13);
+    expect(items[0].quality).toBe(22);
+    expect(items[1].sell_in).toBe(8);
+    expect(items[1].quality).toBe(50);
+    expect(items[2].sell_in).toBe(3);
+    expect(items[2].quality).toBe(50);
+  });
+
+  it("16 updates", () => {
+    const items = []
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+    updateItems(items, 16)
+
+    expect(items[0].sell_in).toBe(-1);
+    expect(items[0].quality).toBe(0);
+    expect(items[1].sell_in).toBe(-6);
+    expect(items[1].quality).toBe(0);
     expect(items[2].sell_in).toBe(-11);
     expect(items[2].quality).toBe(0);
+  })
+})
+
+describe("Conjured", () => {
+
+  it("2 updates", () => {
+    const items = [new Item("Conjured Mana Cake", 3, 13)]
+    updateItems(items, 2)
+
+    expect(items[0].sell_in).toBe(1);
+    expect(items[0].quality).toBe(9);
   });
 
-  it("Sulfuras, Hand of Ragnaros", () => {
-    expect(items[3].sell_in).toBe(0);
-    expect(items[3].quality).toBe(80);
-    expect(items[4].sell_in).toBe(-1);
-    expect(items[4].quality).toBe(80);
-  });
+  it("16 updates", () => {
+    const items = [new Item("Conjured Mana Cake", 3, 13)]
+    updateItems(items, 16)
 
-  it("Backstage passes to a TAFKAL80ETC concert", () => {
-    expect(items[5].sell_in).toBe(-1);
-    expect(items[5].quality).toBe(0);
-    expect(items[6].sell_in).toBe(-6);
-    expect(items[6].quality).toBe(0);
-    expect(items[7].sell_in).toBe(-11);
-    expect(items[7].quality).toBe(0);
-  });
-
-  it("Conjured Mana Cake", () => {
-    expect(items[8].sell_in).toBe(-13);
-    expect(items[8].quality).toBe(0);
-  });
-});
+    expect(items[0].sell_in).toBe(-13);
+    expect(items[0].quality).toBe(0);
+  })
+})
